66ab03bdcc4bfa585e2591f7e9ad9f76bc8a2b84
Jasmeet K Arora <jasmeet@mit.edu>
2014-04-18 15:55:56 -0400

    set universe ids for pincells--this is still a work in progress

--- jasmeet/pincells.py
+++ jasmeet/pincells.py
@@ -18,12 +18,21 @@ pincells = dict() #dictionary of pin cells
 ################################################################################
 
 pincells['16fuel'] = {} #dictionary of cells within this particular pin cell
-pincells['16fuel']['fuel'] = CellBasic(universe=0, material='d1') #initializes inner fuel cell
-pincells['16fuel']['fuel'].addSurface(halfspace=-1, surface=surfaces['Fuel Radius-1'])  
-pincells['16fuel']['clad'] = CellBasic(universe=1, material='d2') #initializes cladding cell
+
+#FUEL
+pincells['uidfuel'] = universe_id() #sets universe ID for fuel
+pincells['16fuel']['fuel'] = CellBasic(universe=pincells['uidfuel'], material='d1') #initializes inner fuel cell
+pincells['16fuel']['fuel'].addSurface(halfspace=-1, surface=surfaces['Fuel Radius-1']) 
+
+#CLADDING
+pincells['uidclad'] = universe_id() #sets universe ID for cladding
+pincells['16fuel']['clad'] = CellBasic(universe=pincells['uidclad'], material='d2') #initializes cladding cell
 pincells['16fuel']['clad'].addSurface(halfspace=+1, surface=surfaces['Fuel Radius-1']) 
 pincells['16fuel']['clad'].addSurface(halfspace=-1, surface=surfaces['Fuel Radius-2'])
-pincells['16fuel']['water'] = CellBasic(universe=2, material='d3') #initializes water cell
+
+#WATER
+pincells['uidwater'] = universe_id() #sets universe ID for water
+pincells['16fuel']['water'] = CellBasic(universe=pincells['uidwater'], material='d3') #initializes water cell
 pincells['16fuel']['water'].addSurface(halfspace=+1, surface=surfaces['Fuel Radius-2']) 
 pincells['16fuel']['water'].addSurface(halfspace=-1, surface=surfaces['Fuel Radius-3'])
 
@@ -31,13 +40,18 @@ pincells['16fuel']['water'].addSurface(halfspace=-1, surface=surfaces['Fuel Radi
 #############################  2.4% Enriched Fuel Pins #########################
 ################################################################################
 
-pincells['fuel'] = {} #dictionary of cells within this particular pin cell
-pincells['24fuel']['fuel'] = CellBasic(universe=0, material='d1') #initializes inner fuel cell
-pincells['24fuel']['fuel'].addSurface(halfspace=-1, surface=surfaces['Fuel Radius-1'])  
-pincells['24fuel']['clad'] = CellBasic(universe=1, material='d2') #initializes cladding cell
+#FUEL
+pincells['24fuel'] = {} #dictionary of cells within this particular pin cell
+pincells['24fuel']['fuel'] = CellBasic(universe=universe_id(), material='d1') #initializes inner fuel cell
+pincells['24fuel']['fuel'].addSurface(halfspace=-1, surface=surfaces['Fuel Radius-1']) 
+
+#CLADDING
+pincells['24fuel']['clad'] = CellBasic(universe=universe_id(), material='d2') #initializes cladding cell
 pincells['24fuel']['clad'].addSurface(halfspace=+1, surface=surfaces['Fuel Radius-1']) 
 pincells['24fuel']['clad'].addSurface(halfspace=-1, surface=surfaces['Fuel Radius-2'])
-pincells['24fuel']['water'] = CellBasic(universe=2, material='d3') #initializes water cell
+
+#WATER
+pincells['24fuel']['water'] = CellBasic(universe=universe_id(), material='d3') #initializes water cell
 pincells['24fuel']['water'].addSurface(halfspace=+1, surface=surfaces['Fuel Radius-2']) 
 pincells['24fuel']['water'].addSurface(halfspace=-1, surface=surfaces['Fuel Radius-3'])"""
 
